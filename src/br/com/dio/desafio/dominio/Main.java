e3f48fabb495a5d450b4eef40947e336f2410c1a
NathaliaCS24 <comercial.nathaliacamargo@gmail.com>
2022-03-09 18:34:59 -0300

    Final

--- src/br/com/dio/desafio/dominio/Main.java
+++ src/br/com/dio/desafio/dominio/Main.java
@@ -23,9 +23,53 @@ public class Main {
         mentoria.setData(LocalDate.now());
 
 
-        System.out.println(curso1);
+        /*System.out.println(curso1);
         System.out.println(curso2);
-        System.out.println(mentoria);
+        System.out.println(mentoria);*/
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devCamila =  new Dev();
+        devCamila.setNome("Camila");
+        devCamila.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos inscritos Camila: " + devCamila.getConteudosInscritos());
+        devCamila.progredir();
+        devCamila.progredir();
+        System.out.println(" ");
+        System.out.println("Conteúdos ainda inscritos Camila: " + devCamila.getConteudosInscritos());
+        System.out.println("Conteúdos concluídos Camila: " + devCamila.getConteudosConcluidos());
+        System.out.println("XP: " + devCamila.calcularTotalXp());
+        System.out.println(" ");
+        if(devCamila.calcularTotalXp() == 150 ) {
+            System.out.println("Você finalizou o Bootcamp com sucesso! Não se esqueça de baixar seu certificado.");
+        } else {
+            System.out.println("Continue fazendo as atividades do Bootcamp!");
+        }
+
+        System.out.println("-------------------------------------------------------------------------------------");
+        System.out.println(" ");
+        Dev devJoao = new Dev();
+        devJoao.setNome("Joao");
+        devJoao.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos inscritos Joao: " + devJoao.getConteudosInscritos());
+        System.out.println(" ");
+        devJoao.progredir();
+        devJoao.progredir();
+        devJoao.progredir();
+        System.out.println("Conteúdos ainda inscritos Joao: " + devJoao.getConteudosInscritos());
+        System.out.println("Conteúdos concluídos Joao: " + devJoao.getConteudosConcluidos());
+        System.out.println("XP: " + devJoao.calcularTotalXp());
+        if(devJoao.calcularTotalXp() == 150 ) {
+            System.out.println("Você finalizou o Bootcamp com sucesso! Não se esqueça de baixar seu certificado.");
+        } else {
+            System.out.println("Continue fazendo as atividades do Bootcamp!");
+        }
+
 
     }
 }
